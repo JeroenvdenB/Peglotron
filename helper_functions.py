7f8a71e91bdcd85e8c60b144c1142ad9eda57265
JeroenvdenB <115401627+JeroenvdenB@users.noreply.github.com>
2023-01-28 22:12:09 +0100

    put add_prompts.py in general helper functions .py

---
+++ helper_functions.py
@@ -0,0 +1,30 @@
+def add_prompt(ctx, msg):
+    """Adds the message content and user to the .csv file.
+    Uses the SUBMISSIONS filepath set in .env 
+    Input: 2 arg: context and the message from discord API.
+    Output: True upon completion."""
+    
+    import pandas as pd
+    import os
+    
+    filepath = os.getenv("SUBMISSIONS") 
+
+    user = ctx.author
+    text = msg.content
+    df = pd.read_csv(filepath, delimiter = ';')
+    add_this = pd.DataFrame([[user, text]], columns=('user','text'))
+    new_df = pd.concat([df, add_this], ignore_index=True) # Don't reference df = pd.concat[df... etc. Gives unbound variable error.
+    new_df.to_csv(path_or_buf=filepath, index=False, sep=';') # Overwrite existing file
+    #print(new_df)
+    
+    return True
+
+def df_to_text(bucket):
+    """Accepts a file bucket name and converts it to a string,
+    suitable for printing in a discord message."""
+
+    import pandas as pd
+    import os
+    filepath = os.getenv(bucket)
+    response_text = pd.read_csv(filepath, delimiter = ';').to_markdown()
+    return response_text
