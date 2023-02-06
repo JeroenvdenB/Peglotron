c3557bc5655198f14eeb8973cdf179a8ff3b15ce
JeroenvdenB <115401627+JeroenvdenB@users.noreply.github.com>
2023-02-06 12:50:42 +0100

    Modal for prompt submission - under construction

---
+++ modals.py
@@ -0,0 +1,53 @@
+import discord
+import pandas as pd
+import os
+
+# A test modal
+class TestModal(discord.ui.Modal):
+  def __init__(self, *args, **kwargs) -> None:
+    super().__init__(*args, **kwargs)
+
+    self.add_item(discord.ui.InputText(label="Enter your prompt here and click submit.", style = discord.InputTextStyle.long))
+  
+  async def callback(self, interaction: discord.Interaction): # DO NOT alter the callback name
+    embed = discord.Embed(title="Modal Results", color=5763719)
+    embed.add_field(name="Suggested prompt:", value = self.children[0].value)
+    print(interaction.user, " has filled in a Modal.") # Don't be a dum-dum and access the variable, not the Class. Thx, me.
+    embed.set_author(name = interaction.user)
+    await interaction.response.send_message(embeds=[embed])
+
+    """ By callin on the interaction variable, several things
+    like content, date, time, user, id, etc. can be accessed.
+    Ideal for passing on important data.
+    
+    Address the fillable fields as children."""
+
+
+# Modal to submit a prompt into any bucket.
+class PromptModal(discord.ui.Modal):
+  def __init__(self, *args, **kwargs) -> None:
+    super().__init__(*args, **kwargs)
+
+    self.val = None
+    self.add_item(discord.ui.InputText(label = "Enter your prompt here and click submit.", style = discord.InputTextStyle.long))
+  
+  async def callback(self, interaction: discord.Interaction):
+    embed = discord.Embed(title = "Prompt submitted for approval :)", color = 5763719)
+    embed.add_field(name = " ", value = self.children[0].value)
+    embed.set_author(name = interaction.user)
+    # Dev lines printing in terminal - remove when done.
+    print(interaction.user, " has submitted a prompt:")
+    print("   ", self.children[0].value)
+    print(self.custom_id)
+
+    await interaction.response.send_message(embeds = [embed])
+
+    # Save interaction to temporary file for processing elsewehere
+    user = interaction.user
+    prompt = self.children[0].value
+    filepath = os.getenv("TEMP")
+    save_this = pd.DataFrame([[user, prompt]], columns = ('user', 'prompt'))
+    # To do: add the new prompt to the awaiting submissions file in the right bucket
+    # Bucket name is passed into the modal as the custom_id
+    # Then check if the temp file is still neccessary
+    
