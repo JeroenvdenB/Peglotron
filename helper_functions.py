4b3372b8623b34227a17c9951c1a88d990699726
JeroenvdenB <115401627+JeroenvdenB@users.noreply.github.com>
2023-02-06 19:24:59 +0100

    re-built the add_prompt function

--- helper_functions.py
+++ helper_functions.py
@@ -1,22 +1,29 @@
-def add_prompt(ctx, msg):
-    """Adds the message content and user to the .csv file.
-    Uses the SUBMISSIONS filepath set in .env 
-    Input: 2 arg: context and the message from discord API.
-    Output: True upon completion."""
-    
-    import pandas as pd
-    import os
-    
-    filepath = os.getenv("SUBMISSIONS") 
+import pandas as pd
+import os
 
-    user = ctx.author
-    text = msg.content
+def add_prompt(user, prompt, bucket):
+    """Adds the user and the submitted prompt to a .csv file
+    for a specific bucket, such as 'SFW' or 'Weekly'. 
+    Input: 3 arg: user id (str), prompt content (str), and bucket (str).
+    Output: True upon completion.
+    Raises a type error if the bucket name is invalid."""
+
+    # Check bucket type
+    if bucket == 'SFW':
+        filepath = os.getenv("OPEN_SFW_SUBMISSIONS")
+    elif bucket == 'NSFW':
+        filepath = os.getenv("OPEN_NSFW_SUBMISSIONS")
+    elif bucket == 'W':
+        filepath = os.getenv("OPEN_WEEKLY_SUBMISSIONS")
+    else:
+        print("Invalid bucket input - known are SFW, NSFW and W.")
+        raise TypeError
+
     df = pd.read_csv(filepath, delimiter = ';')
-    add_this = pd.DataFrame([[user, text]], columns=('user','text'))
-    new_df = pd.concat([df, add_this], ignore_index=True) # Don't reference df = pd.concat[df... etc. Gives unbound variable error.
+    add = pd.DataFrame([[user, prompt]], columns=('user','prompt'))
+    new_df = pd.concat([df, add], ignore_index=True) # Don't reference df = pd.concat[df... etc. Gives unbound variable error.
     new_df.to_csv(path_or_buf=filepath, index=False, sep=';') # Overwrite existing file
-    #print(new_df)
-    
+
     return True
 
 def df_to_text(bucket):
