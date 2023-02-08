18cdf7ffd556256935bad887000f14d8fe4d1994
JeroenvdenB <115401627+JeroenvdenB@users.noreply.github.com>
2023-02-08 10:09:05 +0100

    Improved function help

--- helper_functions.py
+++ helper_functions.py
@@ -1,12 +1,20 @@
 import pandas as pd
 import os
 
-def add_prompt(user, prompt, bucket):
-    """Adds the user and the submitted prompt to a .csv file
-    for a specific bucket, such as 'SFW' or 'Weekly'. 
-    Input: 3 arg: user id (str), prompt content (str), and bucket (str).
-    Output: True upon completion.
-    Raises a type error if the bucket name is invalid."""
+def add_prompt(user: str, prompt: str, bucket: str):
+    """
+    Adds the user and the submitted prompt to a .csv file for a specific bucket, such as 'SFW' or 'Weekly'. 
+    Raises a TypeError in case of invalid bucket notation.
+
+    Parameters
+    ----------
+    name: :class:`str`
+        The user id that should be logged in prompt systems.
+    prompt: :class:`str`
+        The content of the prompt. The Modal that creates this will accept up to 4000 characters.
+    bucket: :class:`str`
+        A denotion for which bucket the prompt goes in. This can be SFW, NSFW or Weekly. It is always put in the 'open' bucket, awaiting admin approval.
+    """
 
     # Check bucket type
     if bucket == 'SFW':
@@ -16,7 +24,7 @@ def add_prompt(user, prompt, bucket):
     elif bucket == 'W':
         filepath = os.getenv("OPEN_WEEKLY_SUBMISSIONS")
     else:
-        print("Invalid bucket input - known are SFW, NSFW and W.")
+        print("Invalid bucket input - accepted are SFW, NSFW and W.")
         raise TypeError
 
     df = pd.read_csv(filepath, delimiter = ';')
@@ -26,9 +34,18 @@ def add_prompt(user, prompt, bucket):
 
     return True
 
-def df_to_text(bucket):
-    """Accepts a file bucket name and converts it to a string,
-    suitable for printing in a discord message."""
+def df_to_text(bucket: str):
+    """
+    Accepts a file bucket name and converts it to a string,
+    suitable for printing in a discord message.
+    It's formatted in markdown to show OK on discord too.
+    
+    Parameters
+    ----------
+    bucket: :class:`str`
+        Input a filepath name from .env. That's different from the prompt storage system, which only uses SFW, NSFW and W for bucket denotion!
+        Since this function can show any .csv content (open, ready and used prompt buckets), you need to be more specific.    
+    """
 
     import pandas as pd
     import os
