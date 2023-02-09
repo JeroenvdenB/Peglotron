21843b35ceb11392ec701d791a9e0873a4c8d74d
JeroenvdenB <115401627+JeroenvdenB@users.noreply.github.com>
2023-02-09 19:50:09 +0100

    Remove test buttons from views

--- views.py
+++ views.py
@@ -1,22 +1,5 @@
 import discord
 from modals import PromptModal
-import pandas as pd
-import os
-
-class MyView(discord.ui.View): # Create a class called Myview that subclasses discord.ui.View
-  @discord.ui.button(label="Click me!", style=discord.ButtonStyle.primary) # Create a blurple button with "click me" on it.
-  async def first_button_callback(self, button, interaction): # Don't give callbacks the same name!
-    await interaction.response.send_message("You clicked the button!") # Send a message when the button is clicked.
-
-  # A second button that will disable all buttons
-  @discord.ui.button(label="Disable", style = discord.ButtonStyle.danger)
-  async def second_button_callback(self, button, interaction):
-    for child in self.children: # Loop over the children of the view
-      child.disabled = True # Set the button to disabled
-    await interaction.response.edit_message(view=self) 
-
-  # Add a time-out here
-  pass
 
 # SUBMISSION BUTTONS VIEW
 # Use the custom_id to send the bucket type into the modal
@@ -39,7 +22,7 @@ class SubmissionButtons(discord.ui.View):
       child.disabled = True
     await interaction.response.edit_message(view= self)
 
-  # Add a time-out here.
+  # Timeout
   async def on_timeout(self):
     for child in self.children:
       child.disabled = True
