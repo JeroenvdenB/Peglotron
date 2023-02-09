07580851f49fbec2dc9b10ec8f7d9fb5c8dd65b8
JeroenvdenB <115401627+JeroenvdenB@users.noreply.github.com>
2023-02-09 19:46:23 +0100

    Removed test modal

--- modals.py
+++ modals.py
@@ -1,27 +1,5 @@
 import discord
-import pandas as pd
-import os
 from helper_functions import add_prompt
-
-# A test modal
-class TestModal(discord.ui.Modal):
-  def __init__(self, *args, **kwargs) -> None:
-    super().__init__(*args, **kwargs)
-
-    self.add_item(discord.ui.InputText(label="Enter your prompt here and click submit.", style = discord.InputTextStyle.long))
-  
-  async def callback(self, interaction: discord.Interaction): # DO NOT alter the callback name
-    embed = discord.Embed(title="Modal Results", color=5763719)
-    embed.add_field(name="Suggested prompt:", value = self.children[0].value)
-    embed.set_author(name = interaction.user.display_name)
-    await interaction.response.send_message(embeds=[embed])
-
-    """ By callin on the interaction variable, several things
-    like content, date, time, user, id, etc. can be accessed.
-    Ideal for passing on important data.
-    
-    Address the fillable fields as children."""
-
 
 # Modal to submit a prompt into any bucket.
 class PromptModal(discord.ui.Modal):
