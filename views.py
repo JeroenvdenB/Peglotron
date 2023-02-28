abcbece0cf997bdb15de22d77aeaac0555ea1d18
JeroenvdenB <115401627+JeroenvdenB@users.noreply.github.com>
2023-02-28 20:44:47 +0100

    added a skip button

--- views.py
+++ views.py
@@ -1,5 +1,6 @@
 import discord
 from modals import PromptModal
+from helper_functions import prompt_to_embed
 
 # SUBMISSION BUTTONS VIEW
 # Use the custom_id to send the bucket type into the modal
@@ -14,7 +15,7 @@ class SubmissionButtons(discord.ui.View):
   
   @discord.ui.button(label="Weekly", style = discord.ButtonStyle.primary)
   async def weekly_button_callback(self, button, interaction):
-    await interaction.response.send_modal(PromptModal(title = "Weekly prompt submission", custom_id = "W"))
+    await interaction.response.send_modal(PromptModal(title = "Weekly prompt submission", custom_id = "WEEKLY"))
   
   @discord.ui.button(label="Stop", style= discord.ButtonStyle.danger)
   async def promptStop_button_callback(self, button, interaction):
@@ -31,11 +32,20 @@ class SubmissionButtons(discord.ui.View):
 
 # APPROVE PROMPT MENU
 class ApprovePrompt(discord.ui.View):
-  @discord.ui.button(label="OK", style = discord.ButtonStyle.success)
+
+  @discord.ui.button(label="Skip", style = discord.ButtonStyle.secondary)
+  async def skip_button_callback(self, button, interaction):
+    index = int(self.message.embeds[0].fields[0].value)
+    index += 1
+    embed = prompt_to_embed("SFW", index)
+    await self.message.delete() # remove original message to keep the channel clutter-free
+    await interaction.response.send_message(content = None, view = self, embeds = [embed]) # don't edit, send a new message! Otherwise self.message does NOT update!
+
+  @discord.ui.button(label="Yay", style = discord.ButtonStyle.success)
   async def ok_button_callback(self, button, interaction):
     await interaction.response.send_message("You pressed the A-OK button!")
   
-  @discord.ui.button(label="X", style=discord.ButtonStyle.danger)
+  @discord.ui.button(label="Nay", style=discord.ButtonStyle.danger)
   async def x_button_callback(self, button, interaction):
     await interaction.response.send_message("You rejected this prompt. Or would have if the button worked.")
 
